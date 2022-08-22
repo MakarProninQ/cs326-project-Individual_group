f37cddcbbb18c079b528e25b584895fb06396dc4
Makar Pronin <mpronin@umass.edu>
2022-08-22 08:16:30 +0500

    fixed git repository

--- server/database.js
+++ server/database.js
@@ -7,7 +7,7 @@ class Database {
     }
 
     async connect() {
-        this.client = MongoClient.connect("mongodb+srv://makarpronin90:makar2003@cluster0.lr3ia8p.mongodb.net/?retryWrites=true&w=majority", {
+        this.client = MongoClient.connect(this.dburl, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             serverApi: ServerApiVersion.v1,
